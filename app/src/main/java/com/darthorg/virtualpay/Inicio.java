b04916b04e990ef14bd0a887cefec9d787a3d966
Wendell Ugalds <wendellugaldsg@gmail.com>
2017-01-31 23:28:04 -0300

    Adicionado fundo e icone ususario

--- app/src/main/java/com/darthorg/virtualpay/Inicio.java
+++ app/src/main/java/com/darthorg/virtualpay/Inicio.java
@@ -1,7 +1,14 @@
 package com.darthorg.virtualpay;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,5 +16,19 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        fullscreenTransparent();
+
+
+    }
+
+    public void fullscreenTransparent(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
     }
 }
