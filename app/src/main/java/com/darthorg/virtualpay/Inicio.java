a14d63f453d1bdb3a41de9532fbf92f7b31b5aa6
Wendell Ugalds <wendellugaldsg@gmail.com>
2017-02-03 23:58:16 -0300

    Grande alteração de layout no app

--- app/src/main/java/com/darthorg/virtualpay/Inicio.java
+++ app/src/main/java/com/darthorg/virtualpay/Inicio.java
@@ -44,9 +44,14 @@ public class Inicio extends AppCompatActivity {
         final TextView valor_cartao_jogador = (TextView) findViewById(R.id.valor_cartao);
 
         final Button btnSorteReves = (Button) findViewById(R.id.btnSorteReves);
-        final Button btnTitulos = (Button) findViewById(R.id.btnTitulos);
+        final Button btnBanco = (Button) findViewById(R.id.btnBanco);
         final Button btnJogadores = (Button) findViewById(R.id.btnJogadores);
 
+        final Button btnPagar = (Button) findViewById(R.id.btnPagar);
+        final Button btnVender = (Button) findViewById(R.id.btnVender);
+        final Button btnComprar = (Button) findViewById(R.id.btnComprar);
+
+        //Dialog SORTE OU REVES
         btnSorteReves.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,13 +59,15 @@ public class Inicio extends AppCompatActivity {
             }
         });
 
-        btnTitulos.setOnClickListener(new View.OnClickListener() {
+        //Dialog BANCO
+        btnBanco.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Titulos Clicado ",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Banco Clicado ",Toast.LENGTH_LONG).show();
             }
         });
 
+        //Dialog JOGADORES
         btnJogadores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,116 +75,83 @@ public class Inicio extends AppCompatActivity {
             }
         });
 
-
-//
-        //btn CARTAO
-        cartao.setOnClickListener(new View.OnClickListener() {
+        //Dialog PAGAR
+        btnPagar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(Inicio.this);
+                AlertDialog.Builder builderPagar = new AlertDialog.Builder(Inicio.this);
 
                 LayoutInflater layoutInflater = getLayoutInflater();
-                final View dialoglayout = layoutInflater.inflate(R.layout.dialog_funcoes_cartao, null);
+                final View dialoglayout = layoutInflater.inflate(R.layout.dialog_funcoes_pagar, null);
 
-                builder.setView(dialoglayout);
+                builderPagar.setView(dialoglayout);
 
-                final FloatingActionButton btnPagar = (FloatingActionButton) dialoglayout.findViewById(R.id.btnPagar);
-                final FloatingActionButton btnVender = (FloatingActionButton) dialoglayout.findViewById(R.id.btnVender);
-                final FloatingActionButton btnComprar = (FloatingActionButton) dialoglayout.findViewById(R.id.btnComprar);
-                final FloatingActionButton btnSeusTitulos = (FloatingActionButton) dialoglayout.findViewById(R.id.btnSeustitulos);
+                final FloatingActionButton btnPagarJogador = (FloatingActionButton) dialoglayout.findViewById(R.id.btnPagarJogador);
+                final FloatingActionButton btnPagarBanco = (FloatingActionButton) dialoglayout.findViewById(R.id.btnPagarBanco);
 
 
 
-                builder.setIcon(getResources().getDrawable(R.drawable.ic_cartao));
-                builder.setTitle(getString(R.string.cartao)+"( " + nome_jogador.getText().toString()+" )\n$ "+valor_cartao_jogador.getText().toString()+getString(R.string.disponivel));
-                builder.setMessage("\n"+getString(R.string.menssagem_dialog_funcao_cartao));
-                final AlertDialog dialog = builder.create();
-
-                //Dialog PAGAR
-                btnPagar.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        AlertDialog.Builder builderPagar = new AlertDialog.Builder(Inicio.this);
-
-                        LayoutInflater layoutInflater = getLayoutInflater();
-                        final View dialoglayout = layoutInflater.inflate(R.layout.dialog_funcoes_pagar, null);
-
-                        builderPagar.setView(dialoglayout);
-
-                        final FloatingActionButton btnPagarJogador = (FloatingActionButton) dialoglayout.findViewById(R.id.btnPagarJogador);
-                        final FloatingActionButton btnPagarBanco = (FloatingActionButton) dialoglayout.findViewById(R.id.btnPagarBanco);
+                builderPagar.setIcon(getResources().getDrawable(R.drawable.ic_cartao));
+                builderPagar.setTitle("Pagar quem?");
+                final AlertDialog dialogPagar = builderPagar.create();
 
 
-
-                        builderPagar.setIcon(getResources().getDrawable(R.drawable.ic_cartao));
-                        builderPagar.setTitle("Pagar quem?");
-                        final AlertDialog dialogPagar = builderPagar.create();
-
-
-                        dialogPagar.show();
-                        dialog.dismiss();
-                    }
-
-                });
-                //Dialog VENDER
-                btnVender.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        AlertDialog.Builder builderVender = new AlertDialog.Builder(Inicio.this);
-
-                        LayoutInflater layoutInflater = getLayoutInflater();
-                        final View dialoglayout = layoutInflater.inflate(R.layout.dialog_funcoes_vender, null);
-
-                        builderVender.setView(dialoglayout);
-
-                        final FloatingActionButton btnVenderCasa = (FloatingActionButton) dialoglayout.findViewById(R.id.btnVenderCasa);
-                        final FloatingActionButton btnHipotecar = (FloatingActionButton) dialoglayout.findViewById(R.id.btnVenderHipotecarTitulos);
-
-
-
-                        builderVender.setIcon(getResources().getDrawable(R.drawable.ic_vender));
-                        builderVender.setTitle("O que você quer vender?");
-                        final AlertDialog dialogVender = builderVender.create();
-
-
-                        dialogVender.show();
-                        dialog.dismiss();
-                    }
-
-                });
-
-                //Dialog COMPRAR
-                btnComprar.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        AlertDialog.Builder builderComprar = new AlertDialog.Builder(Inicio.this);
-
-                        LayoutInflater layoutInflater = getLayoutInflater();
-                        final View dialoglayout = layoutInflater.inflate(R.layout.dialog_funcoes_comprar, null);
-
-                        builderComprar.setView(dialoglayout);
-
-                        final FloatingActionButton btnComprarcTitulos = (FloatingActionButton) dialoglayout.findViewById(R.id.btnComprarcTitulos);
-                        final FloatingActionButton btnComprarCasa = (FloatingActionButton) dialoglayout.findViewById(R.id.btnComprarCasa);
-
-
-
-                        builderComprar.setIcon(getResources().getDrawable(R.drawable.ic_comprar));
-                        builderComprar.setTitle("Gostaria de comprar o que?");
-                        final AlertDialog dialogComprar = builderComprar.create();
-
-
-                        dialogComprar.show();
-                        dialog.dismiss();
-                    }
-
-                });
-
-
-
-                dialog.show();
+                dialogPagar.show();
             }
+
         });
+        //Dialog VENDER
+        btnVender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builderVender = new AlertDialog.Builder(Inicio.this);
+
+                LayoutInflater layoutInflater = getLayoutInflater();
+                final View dialoglayout = layoutInflater.inflate(R.layout.dialog_funcoes_vender, null);
+
+                builderVender.setView(dialoglayout);
+
+                final FloatingActionButton btnVenderCasa = (FloatingActionButton) dialoglayout.findViewById(R.id.btnVenderCasa);
+                final FloatingActionButton btnHipotecar = (FloatingActionButton) dialoglayout.findViewById(R.id.btnVenderHipotecarTitulos);
+
+
+
+                builderVender.setIcon(getResources().getDrawable(R.drawable.ic_vender));
+                builderVender.setTitle("O que você quer vender?");
+                final AlertDialog dialogVender = builderVender.create();
+
+
+                dialogVender.show();
+            }
+
+        });
+
+        //Dialog COMPRAR
+        btnComprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builderComprar = new AlertDialog.Builder(Inicio.this);
+
+                LayoutInflater layoutInflater = getLayoutInflater();
+                final View dialoglayout = layoutInflater.inflate(R.layout.dialog_funcoes_comprar, null);
+
+                builderComprar.setView(dialoglayout);
+
+                final FloatingActionButton btnComprarcTitulos = (FloatingActionButton) dialoglayout.findViewById(R.id.btnComprarcTitulos);
+                final FloatingActionButton btnComprarCasa = (FloatingActionButton) dialoglayout.findViewById(R.id.btnComprarCasa);
+
+
+
+                builderComprar.setIcon(getResources().getDrawable(R.drawable.ic_comprar));
+                builderComprar.setTitle("Gostaria de comprar o que?");
+                final AlertDialog dialogComprar = builderComprar.create();
+
+
+                dialogComprar.show();
+            }
+
+        });
+//
 
 
 
